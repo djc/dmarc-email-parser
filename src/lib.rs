f48945c2f354578f740e28e6a9abeb7d3f743e8a
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-11-07 13:25:12 +0100

    Include From header in report text

--- src/lib.rs
+++ src/lib.rs
@@ -165,6 +165,8 @@ pub enum DmarcResult {
 #[derive(Debug, FromXml)]
 #[xml(rename = "identifiers")]
 pub struct Identifiers {
+    pub envelope_to: Option<String>,
+    pub envelope_from: Option<String>,
     pub header_from: String,
 }
 
